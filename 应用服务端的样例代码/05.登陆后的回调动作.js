1c4f44fba1b3b152af9ec8cb5f35d9d30c716f0e
HY-ZhengWei <HY.ZhengWei@qq.com>
2017-02-06 10:04:58 +0800

    Change-Id: I368b664a7de988cb457b8ee1e32314f62c1456c4 Signed-off-by: HY-ZhengWei <HY.ZhengWei@qq.com>

--- 应用服务端的样例代码/05.登陆后的回调动作.js
+++ 应用服务端的样例代码/05.登陆后的回调动作.js
@@ -1,3 +1,9 @@
+/**
+ * 回调方法。当有多个单点验证服务器时，此方法应执行多次。
+ * 
+ * @param i_SSOServer  单点验证服务器的HostName及端口，如,192.168.1.1:80
+ * @param i_USID       票据号
+ */
 function ssoLogin(i_SSOServer ,i_USID)
 {
 	$.ajax(  
