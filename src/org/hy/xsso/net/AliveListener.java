b46b3ec5c5d0b200d7f498b4267a8b10c27098ca
HY-ZhengWei <HY.ZhengWei@qq.com>
2017-02-28 20:40:27 +0800

    1.0.4 Signed-off-by: HY-ZhengWei <HY.ZhengWei@qq.com>

--- src/org/hy/xsso/net/AliveListener.java
+++ src/org/hy/xsso/net/AliveListener.java
@@ -3,7 +3,9 @@ package org.hy.xsso.net;
 import org.hy.common.Date;
 import org.hy.common.ExpireMap;
 import org.hy.common.Help;
+import org.hy.common.MethodReflect;
 import org.hy.common.net.CommunicationListener;
+import org.hy.common.net.data.Communication;
 import org.hy.common.net.data.CommunicationRequest;
 import org.hy.common.net.data.CommunicationResponse;
 import org.hy.common.xml.XJava;
@@ -74,6 +76,33 @@ public class AliveListener implements CommunicationListener
         {
             v_ResponseData.setResult(1);
             return v_ResponseData;
+        }
+        
+        if ( i_RequestData.getData() == null )
+        {
+            v_ResponseData.setResult(2);
+            return v_ResponseData;
+        }
+        
+        if ( !MethodReflect.isExtendImplement(i_RequestData.getData() ,Communication.class) )
+        {
+            v_ResponseData.setResult(3);
+            return v_ResponseData;
+        }
+        
+        // 设置会话时间
+        Communication v_SessionData = (Communication)i_RequestData.getData();
+        if ( v_SessionData.getSessionTime() == null )
+        {
+            Date v_SessionTime = XJava.getSessionMap().getCreateTime(i_RequestData.getDataXID());
+            if ( v_SessionTime != null )
+            {
+                v_SessionData.setSessionTime(v_SessionTime);
+            }
+            else
+            {
+                v_SessionData.setSessionTime(new Date());
+            }
         }
         
         XJava.putObject(i_RequestData.getDataXID() ,i_RequestData.getData() ,i_RequestData.getDataExpireTimeLen());
