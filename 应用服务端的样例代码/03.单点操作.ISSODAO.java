f7207472c376d637a7c3607c191de9d810448d72
HY-ZhengWei <hy.zhengwei@qq.com>
2018-05-16 14:34:20 +0800

    1.0.6

---
+++ 应用服务端的样例代码/03.单点操作.ISSODAO.java
@@ -0,0 +1,73 @@
+package com.fms.xx.dao;
+
+import com.fms.xx.sys.model.User;
+
+
+
+
+
+/**
+ * 单点登陆DAO接口
+ *
+ * @author      ZhengWei(HY)
+ * @createDate  2017-02-04
+ * @version     v1.0
+ */
+public interface ISSODAO
+{
+    public final  static String               $USID           = "USID";
+    
+    
+    
+    /**
+     * 集群同步单点登陆信息
+     * 
+     * @author      ZhengWei(HY)
+     * @createDate  2017-01-23
+     * @version     v1.0
+     *
+     * @param i_SessionID  会话ID
+     * @param i_User       登陆的用户信息
+     */
+    public void loginClusterUser(String i_SessionID ,User i_User);
+    
+    
+    
+    /**
+     * 用户首次登陆时，集群移除单点登陆信息
+     * 
+     * @author      ZhengWei(HY)
+     * @createDate  2017-01-23
+     * @version     v1.0
+     *
+     * @param i_SessionID  会话ID
+     */
+    public void logoutClusterUser(String i_SessionID);
+    
+    
+    
+    /**
+     * 保持集群会话活力及有效性
+     * 
+     * @author      ZhengWei(HY)
+     * @createDate  2017-02-04
+     * @version     v1.0
+     *
+     * @param i_SessionID  会话ID
+     * @param i_User       登陆的用户信息
+     */
+    public void aliveClusterUser(String i_SessionID ,Object i_User);
+    
+    
+    
+    /**
+     * 当应用服务故障后重启时，同步单点登陆服务器的会话数据
+     * 
+     * @author      ZhengWei(HY)
+     * @createDate  2017-02-07
+     * @version     v1.0
+     *
+     */
+    public void syncSSOSessions();
+    
+}
