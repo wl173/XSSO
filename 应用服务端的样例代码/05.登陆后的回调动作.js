898f6fa55f4cb8c067a6ff3665a2bcaf759de63f
HY-ZhengWei <hy.zhengwei@qq.com>
2018-05-25 08:26:57 +0800

    1.0.6

--- 应用服务端的样例代码/05.登陆后的回调动作.js
+++ 应用服务端的样例代码/05.登陆后的回调动作.js
@@ -6,13 +6,19 @@
  */
 function ssoLogin(i_SSOServer ,i_USID)
 {
-	$.ajax(  
-    {
-        type     : 'get',  
-        url      : 'http://' + i_SSOServer + '/XSSO/sso?USID=' + i_USID,  
-        dataType : 'jsonp',  
-        jsonp    : "jsoncallback",  
-        success  : function(data) {},  
-        error    : function() {}  
-    });
+	if ( i_SSOServer != null && i_SSOServer != undefined && i_SSOServer != "" )
+	{
+		if ( i_USID != null && i_USID != undefined && i_USID != "" )
+		{
+			$.ajax(  
+		    {
+		        type     : 'get',  
+		        url      : 'http://' + i_SSOServer + '/XSSO/sso?USID=' + i_USID,  
+		        dataType : 'jsonp',  
+		        jsonp    : "jsoncallback",  
+		        success  : function(data) {},  
+		        error    : function() {}  
+		    });
+		}
+	}
 }
